c1013a5244c60d94a3541c8a0632c146d0a7cb04
Xiao Ma <irisx.m0809@gmail.com>
2015-03-16 23:58:03 -0400

    add TwoSum

---
+++ Java/TwoSum.java
@@ -0,0 +1,30 @@
+/**
+ * @author XIAO
+ * LeetCode: Two Sum
+ * Description: Given an array of integers and a target,
+ * find two numbers such that they add up to a specific target number.
+ * Return the two indices.
+ */
+
+import java.util.HashMap;
+
+public class TwoSum {
+    public int[] twoSum(int[] numbers, int target) {
+        if (numbers == null || numbers.length ==0 || numbers.length == 1) {
+			return null;
+		}
+		int[] result = new int[2];
+		HashMap<Integer, Integer> h = new HashMap<Integer, Integer>();
+		for (int i = 0; i < numbers.length; i++) {
+			h.put(numbers[i], i);
+		}
+		for (int i = 0; i < numbers.length; i++) {
+			if (h.containsKey(target - numbers[i]) && h.get(target - numbers[i]) != i) {
+				result[0] = i;
+				result[1] = h.get(target - numbers[i]);
+				return result;
+			}
+		}
+		return null;
+    }
+}
