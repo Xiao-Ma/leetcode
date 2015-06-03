a509e1f3f31c7e7cee55939f42e7fcad8ae787cf
Xiao Ma <irisx.m0809@gmail.com>
2015-06-03 15:19:16 -0400

    Generate Parentheses add non-recursive method using array

--- Java/GenerateParentheses.java
+++ Java/GenerateParentheses.java
@@ -25,4 +25,43 @@ public class GenerateParentheses {
             helper(result, s + ")", l, r - 1);
         }
     }
+    /*Non-recursive method.*/
+    public List<String> generateParenthesis2(int n) {
+        List<String> result = new ArrayList<String>();
+        if (n == 0) {
+            return result;
+        }
+        char[] state = new char[2*n];
+        state[0] = '(';
+        int left = 1;
+        int right = 0;
+        int flag = 0;
+        int top = 0;
+        while (top >= 0) {
+            if (left < n && flag == 0) {
+                state[++top] = '(';
+                left++;
+            } else if (right < left && flag <=1) {
+                state[++top] = ')';
+                right++;
+                flag = 0;
+            } else {
+                if (left == n && right == n) {
+                    String tmp = "";
+                    for(int i = 0; i < 2*n; i++) {
+                        tmp += state[i];
+                    }
+                    result.add(tmp);
+                }
+                if (state[top--] == '(') {
+                    flag = 1;
+                    left--;
+                } else {
+                    flag = 2;
+                    right--;
+                }
+            }
+        }
+        return result;
+    }
 }
