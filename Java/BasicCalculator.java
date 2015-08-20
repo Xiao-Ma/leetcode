cfd7997f1087bb3e308d528a262b0cf2cf8ed375
Xiao Ma <irisx.m0809@gmail.com>
2015-08-19 23:57:30 -0400

    leetcode

---
+++ Java/BasicCalculator.java
@@ -0,0 +1,13 @@
+/**
+ * @author Xiao
+ * LeetCode: Basic Calculator
+ * Description: Implement a basic calculator to evaluate a simple expression 
+ *              string. The expression string may contain open ( and closing 
+ *              parentheses ), the plus + or minus sign -, non-negative integers
+ *              and empty spaces. You may assume that the given expression is 
+ *              always valid.
+ */
+ 
+public class BasicCalculator {
+
+}
---
+++ Java/MyStack.java
@@ -0,0 +1,43 @@
+/**
+ * @author Xiao
+ * LeetCode: Implement Stack using Queues
+ * Description: Implement the following operations of a stack using queues.
+ *              push(x) -- Push element x onto stack.
+ *              pop() -- Removes the element on top of the stack.
+ *              top() -- Get the top element.
+ *              empty() -- Return whether the stack is empty.
+ * Notes: You must use only standard operations of a queue -- which means only 
+ *        push to back, peek/pop from front, size, and is empty operations are 
+ *        valid. Depending on your language, queue may not be supported 
+ *        natively. You may simulate a queue by using a list or deque (double-
+ *        ended queue), as long as you use only standard operations of a queue.
+ *        You may assume that all operations are valid (for example, no pop or 
+ *        top operations will be called on an empty stack).
+ */
+ 
+class MyStack {
+    private Queue<Integer> q = new LinkedList<Integer>();
+    private int t;
+    public void push(int x) {
+        q.offer(x);
+        t = x;
+    }
+    public void pop() {
+        int i = 2;
+        int len = q.size();
+        int tmp = q.poll();
+        while (i++ < len) {
+            q.offer(tmp);
+            tmp = q.poll();
+        }
+        t = tmp;
+        q.offer(tmp);
+        q.poll();
+    }
+    public int top() {
+        return t;
+    }
+    public boolean empty() {
+        return q.isEmpty();
+    } 
+}
